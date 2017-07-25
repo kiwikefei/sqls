920ecea03fe78e8e5d45b226c4f7f2f274af4ddb
Andy Tang <kiwikefei@gmail.com>
2017-07-25 14:42:00 +1200

    add course status

---
+++ course_status.sql
@@ -0,0 +1,18 @@
+select 
+	i.id as instance_id,
+	i.name as instance_name,
+	c.name as CourseName,
+	c.id as course_id,
+	u.id as user_id,
+	u.name as user_name,
+	uc.started_at as started_date,
+	(case 
+		when uc.completed_at is null then 'n/a' 
+		else uc.completed_at end) as completed_date
+from user_courses uc
+join users u on uc.user_id = u.id
+join instances i on u.instance_id = i.id
+join courses c on uc.course_id = c.id
+where 
+	uc.started_at > '2017-07-23 00:00:00' and
+	u.id > 15535 
