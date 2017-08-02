42167eb46106d7ac5e9de0a7b88cbdd4e77ab7a2
Andy Tang <kiwikefei@gmail.com>
2017-08-02 14:55:07 +1200

    add background check for actived user report

--- actived_user_per_month_per_instance.sql
+++ actived_user_per_month_per_instance.sql
@@ -10,7 +10,12 @@ join(
 	where uc.started_at between '2017-06-01 00:00:00' and '2017-06-30 00:00:00'
 	union
 	select distinct id as user_id from users u
-	where u.created_at between '2017-06-01 00:00:00' and '2017-06-30 00:00:00') 
+	where u.created_at between '2017-06-01 00:00:00' and '2017-06-30 00:00:00'
+	union 
+	select distinct user_id from backgrounds bg
+	where bg.updated_at between '2017-06-01 00:00:00' and '2017-06-30 00:00:00'
+	) 
  as actived on actived.user_id = u.id
 group by i.id
+
 
