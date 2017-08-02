ffd6c628c5608018352a5fec7709e9854684a293
Andy Tang <kiwikefei@gmail.com>
2017-08-02 14:34:02 +1200

    format sqlfile

--- actived_user_per_month_per_instance.sql
+++ actived_user_per_month_per_instance.sql
@@ -1,13 +1,16 @@
-select i.name ,count(actived.user_id) from instances i
+select 
+	i.name ,count(actived.user_id) 
+from instances i
 join users u on u.instance_id = i.id
-join 
- (select distinct user_id from pitches p
-where p.created_at between '2017-06-01 00:00:00' and '2017-06-30 00:00:00'
-union
-select distinct user_id from user_courses uc
-where uc.started_at between '2017-06-01 00:00:00' and '2017-06-30 00:00:00'
-union
-select distinct id as user_id from users u
-where u.created_at between '2017-06-01 00:00:00' and '2017-06-30 00:00:00') as actived on actived.user_id = u.id
+join(
+	select distinct user_id from pitches p 
+	where p.created_at between '2017-06-01 00:00:00' and '2017-06-30 00:00:00'
+ 	union
+	select distinct user_id from user_courses uc
+	where uc.started_at between '2017-06-01 00:00:00' and '2017-06-30 00:00:00'
+	union
+	select distinct id as user_id from users u
+	where u.created_at between '2017-06-01 00:00:00' and '2017-06-30 00:00:00') 
+ as actived on actived.user_id = u.id
 group by i.id
 
